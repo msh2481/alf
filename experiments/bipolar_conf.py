590072a078d2ba1b583867616fa0cc6f569693e4
Mikhail Budnikov <msh24819@gmail.com>
2025-11-28 14:36:19 +0100

    check again on bipolar

---
+++ experiments/bipolar_conf.py
@@ -0,0 +1,106 @@
+# Copyright (c) 2025 Horizon Robotics and ALF Contributors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+import alf
+
+from alf.algorithms.seed_sampling import SeedDqnAlgorithm, SeedSacAlgorithm
+from alf.algorithms.sac_algorithm import SacAlgorithm
+from alf.algorithms.dqn_algorithm import DqnAlgorithm
+from alf.algorithms.action_repulsion_algorithm import ActionRepulsionAlgorithm
+from alf.networks import QNetwork, QNetworkBase, DebugLinearQNetwork, RandomizedPriorQNetwork, OptimisticQNetwork
+from alf.networks.encoding_networks import IdentityEncodingNetwork, EncodingNetwork
+from alf.utils.losses import element_wise_squared_loss
+from alf.environments.simple.randomized_bipolar_chain import RandomizedBipolarChain
+
+NUM_COPIES = 4
+BATCH_SIZE = 1024 * NUM_COPIES
+ENV_COUNTS = NUM_COPIES
+UNROLL_LENGTH = 1
+MINI_BATCH_LENGTH = 2
+SEED_VERSION = True
+
+HIDDEN_LAYERS = (256, )
+
+PRIOR_SCALE = 1.0
+PARAMETER_TARGET_STD = 0.0
+PARAMETER_TARGET_ALPHA = 0.0
+REWARD_NOISE_STD = 0.0
+assert (PRIOR_SCALE == 0.0) or (
+    PARAMETER_TARGET_STD == 0.0 and PARAMETER_TARGET_ALPHA == 0.0
+), "Don't turn on both PRIOR_SCALE and PARAMETER_TARGET_STD/PARAMETER_TARGET_ALPHA"
+
+# environment config
+alf.config('create_environment',
+           env_name="RandomizedBipolarChain-v0",
+           num_parallel_environments=ENV_COUNTS)
+
+alf.config('QNetwork', fc_layer_params=HIDDEN_LAYERS)
+alf.config('OptimisticQNetwork', init_mean=0.0, init_std=1e-3)
+alf.config('RandomizedPriorQNetwork',
+           network_ctor=QNetwork,
+           prior_scale=PRIOR_SCALE)
+
+alf.config(
+    'SeedDqnAlgorithm' if SEED_VERSION else 'DqnAlgorithm',
+    rollout_epsilon_greedy=0.0,
+    q_network_cls=RandomizedPriorQNetwork,
+    parameter_target_std=PARAMETER_TARGET_STD,
+    parameter_target_alpha=PARAMETER_TARGET_ALPHA,
+    reward_noise_std=REWARD_NOISE_STD,
+)
+alf.config(
+    'SacAlgorithm',
+    target_update_tau=0.05,
+    target_update_period=1,
+)
+
+alf.config('OneStepTDLoss',
+           td_error_loss_fn=element_wise_squared_loss,
+           gamma=0.9)
+
+alf.config(
+    "ActionRepulsionAlgorithm",
+    algorithm_ctor=SeedDqnAlgorithm if SEED_VERSION else DqnAlgorithm,
+    optimizer=alf.optimizers.Adam(lr=5e-3, name='main'),
+    num_copies=NUM_COPIES,
+    batch_size=BATCH_SIZE,
+    env_counts=ENV_COUNTS,
+    unroll_length=UNROLL_LENGTH,
+    mini_batch_length=MINI_BATCH_LENGTH,
+    # repulsion_num_obs=100,
+    log_every_n_steps=200,
+    use_exploration_seeds=SEED_VERSION,
+    env_class=RandomizedBipolarChain,
+    video_record_interval=None,
+)
+
+# training config
+alf.config('TrainerConfig',
+           algorithm_ctor=ActionRepulsionAlgorithm,
+           initial_collect_steps=200,
+           mini_batch_length=MINI_BATCH_LENGTH,
+           mini_batch_size=BATCH_SIZE,
+           unroll_length=UNROLL_LENGTH,
+           num_updates_per_train_iter=1,
+           num_iterations=10000,
+           num_checkpoints=3,
+           evaluate=False,
+           eval_interval=100,
+           replay_buffer_length=10000,
+           random_seed=42,
+           whole_replay_buffer_training=False,
+           clear_replay_buffer=False,
+           summarize_grads_and_vars=False,
+           debug_summaries=False,
+           summary_interval=100,
+           summarize_first_interval=False)
