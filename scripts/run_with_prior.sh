610da8a4330b2cc2d4ce11f0813d45732a7a0b3a
Mikhail Budnikov <msh24819@gmail.com>
2026-02-08 18:09:06 +0100

    new scripts

---
+++ scripts/run_with_prior.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+bash scripts/run_bipolar.sh \
+    CONF="experiments/bipolar_conf.py" \
+    ENV="BipolarChain-medium-sparse-onehot-discrete-v0" \
+    LR="0.05" \
+    WD="1e-5" \
+    GAMMA="0.95" \
+    PRIOR_SCALE="0.5" \
+    ALPHA="1e-2" \
+    TAU="0.5" \
+    UTD=4 \
+    NUM_AGENTS=1 \
+    ASYNC="False" \
+    ENTROPY_REWARD="False" \
+    N_COMPONENTS=500 \
+    SEEDS="" \
+    BASE_DIR="" \
+    NAME="with_prior" \
+    EXTRA_ARGS=""
---
+++ scripts/run_without_prior.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+bash scripts/run_bipolar.sh \
+    CONF="experiments/bipolar_conf.py" \
+    ENV="BipolarChain-medium-sparse-onehot-discrete-v0" \
+    LR="0.05" \
+    WD="1e-5" \
+    GAMMA="0.95" \
+    PRIOR_SCALE="0" \
+    ALPHA="0" \
+    TAU="0.05" \
+    UTD=4 \
+    NUM_AGENTS=1 \
+    ASYNC="False" \
+    ENTROPY_REWARD="False" \
+    N_COMPONENTS=500 \
+    SEEDS="" \
+    BASE_DIR="" \
+    NAME="without_prior" \
+    EXTRA_ARGS=""
